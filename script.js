edd07acff7f928680f4e71503bae35b49e1e23ae
devanshagarwal0 <122717192+devanshagarwal0@users.noreply.github.com>
2024-09-16 18:16:13 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,59 @@
-let speech =new SpeechSynthesisUtterance();
+function start() {
+    let Timer = 60;
+    let score = 0;
+    let hitnum = 0;
 
-let voices=[];
-let voiceSelect = document.querySelector("select");
+    function makeBubble() {
+        var clutter = "";
+        for (var i = 1; i < 125; i++) {
+            var rn = Math.round(Math.random() * 10);
+            clutter += `<div class="bubble">${rn}</div>`;
+        }
+        document.querySelector('#pbtm').innerHTML = clutter;
+    }
 
-window.speechSynthesis.onvoiceschanged = () =>{
-    voices = window.speechSynthesis.getVoices();
-    speech.voice=voices[0];
+    function runTimer() {
+        let timeint = setInterval(function () {
+            if (Timer > 0) {
+                Timer--;
+                document.querySelector("#timer").textContent = Timer;
+            } else {
+                document.querySelector("#pbtm").removeEventListener("click", handleBubbleClick);
+                document.querySelector("#pbtm").innerHTML = `
+                    <h1 id="end">Score: ${score} <br>GAME OVER
+                    <button onclick="start()">
+                        <img src="images/restarting.png" height="50px" width="50px" style="border-radius:50%;"> Restart
+                    </button>
+                    </h1> `;
+                clearInterval(timeint);
+            }
+        }, 1000);
+    }
 
-    voices.forEach((voice,i) =>(voiceSelect.options[i]=new Option(voice.name,i)));
-};
+    function getNewHit() {
+        hitnum = Math.round(Math.random() * 10);
+        document.querySelector("#newhit").textContent = hitnum;
+    }
 
-voiceSelect.addEventListener("change",() =>{
-    speech.voice=voices[voiceSelect.value];
-})
+    function increaseScore() {
+        score += 10;
+        document.querySelector("#newscore").textContent = score;
+    }
 
+    function handleBubbleClick(dets) {
+        let clicked = Number(dets.target.textContent);
+        if (clicked === hitnum) {
+            increaseScore();
+            getNewHit();
+            makeBubble();
+        } else {
+            makeBubble();
+        }
+    }
 
-
-document.querySelector("button").addEventListener("click",() =>{
-    speech.text = document.querySelector("textarea").value;
-    window.speechSynthesis.speak(speech);
-})
+    document.querySelector("#pbtm").addEventListener("click", handleBubbleClick);
+    
+    runTimer();
+    getNewHit();
+    makeBubble();
+}
